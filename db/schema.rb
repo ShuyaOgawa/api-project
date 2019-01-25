962d9c79aeafa6828e34677e02b04c9551dcb1e6
小川秀哉 <shuya464924@gmail.com>
2019-01-25 19:31:15 +0900

    add password column

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_21_125411) do
+ActiveRecord::Schema.define(version: 2019_01_25_103011) do
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
+    t.string "password"
   end
 
 end
